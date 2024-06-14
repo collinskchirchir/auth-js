2b83aa5d71e0bb52804f5fe2300e10871117f8c0
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-14 11:54:21 +0300

    integrated login server action

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,53 +1,65 @@
-'use client'
-import * as z from 'zod'
-import React from 'react';
+'use client';
+import * as z from 'zod';
+import React, { useState, useTransition } from 'react';
 import CardWrapper from '@/components/auth/card-wrapper';
 import { useForm } from 'react-hook-form';
-import { LoginSchema } from '@/shemas';
+import { LoginSchema } from '@/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { FormError } from '@/components/form-error';
 import { FormSuccess } from '@/components/form-success';
+import { login } from '@/actions/login';
 
 export const LoginForm = () => {
-
+  const [error, setError] = useState<string | undefined>('');
+  const [success, setSuccess] = useState<string | undefined>('');
+  const [isPending, startTransistion] = useTransition();
   const form = useForm<z.infer<typeof LoginSchema>>({
     resolver: zodResolver(LoginSchema),
     defaultValues: {
       email: '',
-      password: ''
-    }
-  })
+      password: '',
+    },
+  });
 
   const onSubmit = (values: z.infer<typeof LoginSchema>) => {
-    console.log(values)
-  }
+    setError('');
+    setSuccess('');
+    startTransistion(() => {
+      login(values)
+        .then((data) => {
+          setError(data.error);
+          setSuccess(data.success);
+        });
+    });
+  };
   return (
     <CardWrapper
-      headerLabel='Welcome Back'
+      headerLabel="Welcome Back"
       backButtonLabel="Don't have an account?"
-      backButtonHref='/auth/register'
+      backButtonHref="/auth/register"
       showSocial
     >
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className='space-y-6'
+          className="space-y-6"
         >
-          <div className='space-y-4'>
+          <div className="space-y-4">
             <FormField
               control={form.control}
-              name='email'
-              render={({field}) => (
+              name="email"
+              render={({ field }) => (
                 <FormItem>
                   <FormLabel>Email</FormLabel>
                   <FormControl>
                     <Input
                       {...field}
-                      placeholder='john.doe@example.com'
-                      type='email'
+                      disabled={isPending}
+                      placeholder="john.doe@example.com"
+                      type="email"
                     />
                   </FormControl>
                   <FormMessage />
@@ -56,15 +68,16 @@ export const LoginForm = () => {
             />
             <FormField
               control={form.control}
-              name='password'
-              render={({field}) => (
+              name="password"
+              render={({ field }) => (
                 <FormItem>
                   <FormLabel>Password</FormLabel>
                   <FormControl>
                     <Input
                       {...field}
-                      placeholder='******'
-                      type='password'
+                      disabled={isPending}
+                      placeholder="******"
+                      type="password"
                     />
                   </FormControl>
                   <FormMessage />
@@ -72,11 +85,12 @@ export const LoginForm = () => {
               )}
             />
           </div>
-          <FormError message=''/>
-          <FormSuccess message=''/>
+          <FormError message={error} />
+          <FormSuccess message={success} />
           <Button
-            type='submit'
-            className='w-full'
+            type="submit"
+            className="w-full"
+            disabled={isPending}
           >
             Login
           </Button>
