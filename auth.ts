a7f328cfed3ac645741e04ffaa75d1b87de47f8c
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-16 22:20:48 +0300

    auth.js session and token callback

--- auth.ts
+++ auth.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@prisma/client';
 import GitHub from 'next-auth/providers/github';
 import Credentials from '@auth/core/providers/credentials';
 import { LoginSchema } from '@/schema';
-import { getUserByEmail } from '@/data/user';
+import { getUserByEmail, getUserById } from '@/data/user';
 import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient()
@@ -12,6 +12,27 @@ const prisma = new PrismaClient()
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
+  callbacks: {
+    async session({token, session}){
+      console.log({
+        sessionToken: token
+      })
+      if(token.sub && session.user){
+      session.user.id = token.sub
+      }
+      if(token.role && session.user){
+        session.user.role = token.role
+      }
+      return session
+    },
+    async jwt({token}){
+      if(!token.sub) return token
+      const existingUser = await getUserById(token.sub)
+      if(!existingUser) return token
+      token.role = existingUser.role
+      return token
+    }
+  },
   providers: [
     Credentials({
       async authorize(credentials) {
