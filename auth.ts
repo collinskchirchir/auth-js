e3a9018f3427bcb62c44ad2aedafd0ab9c0beebc
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-16 22:55:27 +0300

    user type extended for fetching user role in auth callback

--- auth.ts
+++ auth.ts
@@ -1,56 +1,64 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient } from '@prisma/client';
-import GitHub from 'next-auth/providers/github';
+import { PrismaClient, UserRole } from '@prisma/client';
 import Credentials from '@auth/core/providers/credentials';
 import { LoginSchema } from '@/schema';
 import { getUserByEmail, getUserById } from '@/data/user';
 import bcrypt from 'bcryptjs';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
   session: { strategy: 'jwt' },
   callbacks: {
-    async session({token, session}){
-      console.log({
-        sessionToken: token
-      })
-      if(token.sub && session.user){
-      session.user.id = token.sub
+    /** Prevent signin if email is not verified
+    async signIn({user}){
+      const existingUser = await getUserById(user.id)
+      if(!existingUser || !existingUser.emailVerified){
+        return false
       }
-      if(token.role && session.user){
-        session.user.role = token.role
-      }
-      return session
+      return true
     },
-    async jwt({token}){
-      if(!token.sub) return token
-      const existingUser = await getUserById(token.sub)
-      if(!existingUser) return token
-      token.role = existingUser.role
-      return token
-    }
+    */
+    async session({ token, session }) {
+      console.log({
+        sessionToken: token,
+      });
+      if (token.sub && session.user) {
+        session.user.id = token.sub;
+      }
+      if (token.role && session.user) {
+        session.user.role = token.role as UserRole
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+      if (!existingUser) return token;
+      token.role = existingUser.role;
+      return token;
+    },
   },
   providers: [
     Credentials({
       async authorize(credentials) {
         const validatedFields = LoginSchema.safeParse(credentials);
         if (validatedFields.success) {
-          const {email, password} = validatedFields.data;
+          const { email, password } = validatedFields.data;
 
-          const user = await getUserByEmail(email)
-          if(!user || !user.password) return null
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
 
           const passwordsMatch = await bcrypt.compare(
             password,
-            user.password
-          )
-          if(passwordsMatch) return user;
+            user.password,
+          );
+          if (passwordsMatch) return user;
         }
         return null;
-      }
-    })
+      },
+    }),
   ],
 });
