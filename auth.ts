45d67016630e68cbce90609638db6e3da69a3484
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-17 16:39:28 +0300

    fix: removed edge runtime config

--- auth.ts
+++ auth.ts
@@ -1,26 +1,23 @@
 import NextAuth from 'next-auth';
 import { PrismaAdapter } from '@auth/prisma-adapter';
-import { PrismaClient, UserRole } from '@prisma/client';
-import Credentials from '@auth/core/providers/credentials';
-import { LoginSchema } from '@/schema';
-import { getUserByEmail, getUserById } from '@/data/user';
-import bcrypt from 'bcryptjs';
-
-const prisma = new PrismaClient();
+import { UserRole } from '@prisma/client';
+import { getUserById } from '@/data/user';
+import { db } from '@/lib/db';
+import authConfig from '@/auth.config';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(db),
   session: { strategy: 'jwt' },
   callbacks: {
     /** Prevent signin if email is not verified
-    async signIn({user}){
-      const existingUser = await getUserById(user.id)
-      if(!existingUser || !existingUser.emailVerified){
-        return false
-      }
-      return true
-    },
-    */
+     async signIn({user}){
+     const existingUser = await getUserById(user.id)
+     if(!existingUser || !existingUser.emailVerified){
+     return false
+     }
+     return true
+     },
+     */
     async session({ token, session }) {
       console.log({
         sessionToken: token,
@@ -29,7 +26,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         session.user.id = token.sub;
       }
       if (token.role && session.user) {
-        session.user.role = token.role as UserRole
+        session.user.role = token.role as UserRole;
       }
       return session;
     },
@@ -37,28 +34,10 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       if (!token.sub) return token;
       const existingUser = await getUserById(token.sub);
       if (!existingUser) return token;
+      // @ts-ignore
       token.role = existingUser.role;
       return token;
     },
   },
-  providers: [
-    Credentials({
-      async authorize(credentials) {
-        const validatedFields = LoginSchema.safeParse(credentials);
-        if (validatedFields.success) {
-          const { email, password } = validatedFields.data;
-
-          const user = await getUserByEmail(email);
-          if (!user || !user.password) return null;
-
-          const passwordsMatch = await bcrypt.compare(
-            password,
-            user.password,
-          );
-          if (passwordsMatch) return user;
-        }
-        return null;
-      },
-    }),
-  ],
+  ...authConfig,
 });
