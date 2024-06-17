0bd4da83551f7af539ad406d354999f14f74a6ba
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-17 16:43:59 +0300

    OAuth Google and Github config

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,38 @@
 import GitHub from 'next-auth/providers/github';
 import type { NextAuthConfig } from 'next-auth';
+import Google from 'next-auth/providers/google';
+import Credentials from '@auth/core/providers/credentials';
+import { LoginSchema } from '@/schema';
+import { getUserByEmail } from '@/data/user';
+import bcrypt from 'bcryptjs';
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+export default {
+  providers: [
+    Google({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+    Credentials({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordsMatch = await bcrypt.compare(
+            password,
+            user.password,
+          );
+          if (passwordsMatch) return user;
+        }
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,18 +1,24 @@
 'use client';
+import { signIn } from 'next-auth/react';
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
+import { DEFAULT_LOGIN_REDIRECT } from '@/routes';
 
 export const Social = () => {
+  const onClick = (provider: 'google' | 'github') => {
+    signIn(provider, {
+      callbackUrl: DEFAULT_LOGIN_REDIRECT,
+    });
+  };
   return (
-    <div className="flex items-center w-full gap-x-2">
+    <div className="flex w-full items-center gap-x-2">
       <Button
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {
-        }}
+        onClick={() => onClick('google')}
       >
         <FcGoogle className="size-5" />
       </Button>
@@ -20,8 +26,7 @@ export const Social = () => {
         size="lg"
         className="w-full"
         variant="outline"
-        onClick={() => {
-        }}
+        onClick={() => onClick('github')}
       >
         <FaGithub className="size-5" />
       </Button>
