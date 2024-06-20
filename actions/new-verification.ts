865d92a96932335ea748e7d5287fc7de9c61bf8e
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-20 11:33:07 +0300

    fix: verification token record deleted after successful confirmation

--- actions/new-verification.ts
+++ actions/new-verification.ts
@@ -1,33 +1,33 @@
-'use server'
-import {db} from '@/lib/db'
-import {getUserByEmail} from '@/data/user';
-import { getVerificationTokenByEmail, getVerificationTokenByToken } from '@/data/verification-token';
+'use server';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
+import { getVerificationTokenByToken } from '@/data/verification-token';
 
 export const newVerification = async (token: string) => {
-  const existingToken = await getVerificationTokenByToken(token)
+  const existingToken = await getVerificationTokenByToken(token);
   if (!existingToken) {
-    return {error: 'Token does not exist'}
+    return { error: 'Token does not exist' };
   }
-  const hasExpired = new Date(existingToken.expires) < new Date()
-  if (hasExpired){
-    return {error: 'Token has expired!'}
+  const hasExpired = new Date(existingToken.expires) < new Date();
+  if (hasExpired) {
+    return { error: 'Token has expired!' };
   }
 
   const existingUser = await getUserByEmail(existingToken.email);
   if (!existingUser) {
-    return {error: 'Email does not exist!'}
+    return { error: 'Email does not exist!' };
   }
   await db.user.update({
-    where: {id: existingUser.id},
+    where: { id: existingUser.id },
     data: {
       emailVerified: new Date(),
       email: existingToken.email, // reuse when user update email
-     }
-  })
+    },
+  });
   // remove verification token
   await db.verificationToken.delete({
-    where: {id: existingUser.id},
-  })
+    where: { id: existingToken.id },
+  });
 
-  return {success: 'Email verified!'}
-}
+  return { success: 'Email verified!' };
+};
--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,8 +1,8 @@
-'use client'
-import {useCallback, useEffect, useState} from 'react'
-import {BeatLoader} from 'react-spinners'
+'use client';
+import { useCallback, useEffect, useState } from 'react';
+import { BeatLoader } from 'react-spinners';
 
-import {newVerification} from '@/actions/new-verification';
+import { newVerification } from '@/actions/new-verification';
 import CardWrapper from '@/components/auth/card-wrapper';
 import { useSearchParams } from 'next/navigation';
 import { FormSuccess } from '@/components/form-success';
@@ -15,32 +15,34 @@ export const NewVerificationForm = () => {
   const token = searchParams.get('token');
   const onSubmit = useCallback(() => {
     if (!token) {
-      setError("Missing token!")
-      return
+      setError('Missing token!');
+      return;
     }
     newVerification(token)
       .then((data) => {
-        setSuccess(data.success)
-        setError(data.error)
+        setSuccess(data.success);
+        setError(data.error);
       })
-      .catch(()=> {
-        setError('Something went wrong!')
-      })
-  },[token])
+      .catch(() => {
+        setError('Something went wrong!');
+      });
+  }, [token]);
   useEffect(() => {
-    onSubmit()
+    onSubmit();
   }, [onSubmit]);
   return (
     <CardWrapper
-      headerLabel='Confirming your verification'
-      backButtonLabel='Back to login'
-      backButtonHref='/auth/login'
+      headerLabel="Confirming your verification"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
     >
-     <div className='flex w-full items-center justify-center'>
-        <BeatLoader />
-       <FormSuccess message={success} />
-       <FormError message={error} />
-     </div>
+      <div className="flex w-full items-center justify-center">
+        {!success && !error && (
+          <BeatLoader />
+        )}
+        <FormSuccess message={success} />
+        <FormError message={error} />
+      </div>
     </CardWrapper>
-  )
-}
+  );
+};
