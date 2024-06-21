a2e05a3e1ac79e403a433f7059be3e360693a5bb
Collins Chirchir <collinskchirchir@gmail.com>
2024-06-21 12:19:56 +0300

    feat: password reset token

---
+++ app/auth/reset/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ResetForm } from '@/components/auth/reset-form';
+
+const PasswordResetPage = () => {
+  return (
+    <ResetForm />
+  );
+};
+
+export default PasswordResetPage;
--- components/auth/reset-form.tsx
+++ components/auth/reset-form.tsx
@@ -3,39 +3,34 @@ import * as z from 'zod';
 import React, { useState, useTransition } from 'react';
 import CardWrapper from '@/components/auth/card-wrapper';
 import { useForm } from 'react-hook-form';
-import { LoginSchema } from '@/schema';
+import { ResetSchema } from '@/schema';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 import { FormError } from '@/components/form-error';
 import { FormSuccess } from '@/components/form-success';
-import { login } from '@/actions/login';
 import { LuLoader2 } from 'react-icons/lu';
-import { useSearchParams } from 'next/navigation';
-import Link from 'next/link';
+import { reset } from '@/actions/reset';
 
-export const LoginForm = () => {
-  const searchParams = useSearchParams();
-  const urlError = searchParams.get('error') === 'OAuthAccountNotLinked'
-    ? 'Email already in use with a different provider!'
-    : '';
+export const ResetForm = () => {
+
   const [error, setError] = useState<string | undefined>('');
   const [success, setSuccess] = useState<string | undefined>('');
   const [isPending, startTransition] = useTransition();
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof ResetSchema>>({
+    resolver: zodResolver(ResetSchema),
     defaultValues: {
       email: '',
-      password: '',
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof ResetSchema>) => {
     setError('');
     setSuccess('');
+    console.log(values);
     startTransition(() => {
-      login(values)
+      reset(values)
         .then((data) => {
           setError(data?.error);
           //   TODO: Add when we add 2FA
@@ -45,10 +40,9 @@ export const LoginForm = () => {
   };
   return (
     <CardWrapper
-      headerLabel="Welcome Back"
-      backButtonLabel="Don't have an account?"
-      backButtonHref="/auth/register"
-      showSocial
+      headerLabel="Forgot your password?"
+      backButtonLabel="Back to login"
+      backButtonHref="/auth/login"
     >
       <Form {...form}>
         <form
@@ -74,36 +68,8 @@ export const LoginForm = () => {
                 </FormItem>
               )}
             />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Password</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      disabled={isPending}
-                      placeholder="******"
-                      type="password"
-                    />
-                  </FormControl>
-                  <Button
-                    size="sm"
-                    variant="link"
-                    asChild
-                    className="px-0 font-normal"
-                  >
-                    <Link href="/auth/reset">
-                      Forgot password?
-                    </Link>
-                  </Button>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
           </div>
-          <FormError message={error || urlError} />
+          <FormError message={error} />
           <FormSuccess message={success} />
           <Button
             type="submit"
@@ -116,10 +82,10 @@ export const LoginForm = () => {
                   className="mr-2 size-4 animate-spin"
                   aria-hidden="true"
                 />
-                Logging in...
+                Reseting...
               </>
             ) : (
-              <>Login</>
+              <>Send reset email</>
             )}
           </Button>
         </form>
